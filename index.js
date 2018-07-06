f661ae4fc2916554ea779773e9d0a04eb342802d
Yoni Rabinovitch <yrabinovitch@tycoint.com>
2018-07-06 11:04:38 +0300

    Initial working version. Not capturing stderr logs for some reason

--- index.js
+++ index.js
@@ -1,34 +1,35 @@
-const {Docker} = require('node-docker-api');
+const {Docker} = require('node-docker-api'),
+            fs = require('fs');
+
+// We should log a container if it has a logging strategy in its Labels            
+const shouldLog = container => 'io.logmux.strategy' in container.data.Labels;
+
+// Collect logs from a container
+const logs = container => container.logs({follow: true, stdout: true, stderr: true});
 
 const attachToContainers = async () => {
 
   try {
     const docker = new Docker({ socketPath: '/var/run/docker.sock' });
 
+    let logFile = fs.createWriteStream('/mnt/messages');
+
     let containers = await docker.container.list();
-    //let inspections = await Promise.all(containers.map(container=>container.status()));
-    
-    //console.log(`========Found ${inspections.length} inspections`) ; 
-    //inspections.map(inspection => console.log(inspection));
 
     console.log(`========Found ${containers.length} containers`) ; 
     containers.map(container => console.log(container.data.Labels));
     console.log(`=======================================================`)
     
     let attachees = 
-      containers.filter(container => 'io.logmux.strategy' in container.data.Labels);
+      containers.filter(shouldLog);
  
-    let streams = await Promise.all
-      (attachees
-        .map(attachee=>attachee
-          .logs({follow: true,
-                  stdout: true,
-                  stderr: true})));
+    let streams = await Promise.all(attachees.map(logs));
                  
-    streams.map(stream => {
-      stream.on('data', info => console.log(info.toString()));
-      stream.on('error', err => console.log(err.toString()));
-    })              
+    streams.map(stream => stream.pipe(logFile));
+
+      //stream.on('data', info => console.log(info.toString()));
+      //stream.on('error', err => console.log(err.toString()));
+    //})              
       
   } catch (err) {
       console.error(`attachToContainers: ${err.message}`);
